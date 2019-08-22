7c1ed83abbb45ca5b80d29ff92518a5221f46cf8
qiaoyuhu <yuhuqiao@163.com>
2019-08-22 17:42:07 +0800

    add 百度.html String/

---
+++ String/clear.cpp
@@ -0,0 +1,25 @@
+/*************************************************************************
+	> File Name: clear.cpp
+	> Author: qiaoyuhu
+	> Mail: yuhuqiao@163.com 
+	> Created Time: 2019年08月21日 星期三 17时42分07秒
+ ************************************************************************/
+
+#include <iostream>
+#include <string.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int a =10;
+	double b = 2.8;
+	string s1;
+	string a_str;
+	s1 = to_string(a);
+	a_str = s1;
+	s1.clear();
+	cout << s1 <<endl;
+	
+
+}
